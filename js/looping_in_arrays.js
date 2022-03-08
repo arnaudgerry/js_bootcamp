5f864268c07cf61c13cc0c2502f118f3f84c47d1
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-08 12:40:57 +0300

    ft: for loop

--- js/looping_in_arrays.js
+++ js/looping_in_arrays.js
@@ -25,8 +25,33 @@
 // alert(doubledNumbers);
 
 //Double numbers in an array using .map()
-let numbers = [1, 2, 3, 4, 5];
-let doubledNumbers = numbers.map(function(number) {
-    return number * 2;
-});
-alert(doubledNumbers);
+// let numbers = [1, 2, 3, 4, 5];
+// let doubledNumbers = numbers.map(function(number) {
+//     return number * 2;
+// });
+// alert(doubledNumbers);
+
+//Looping with "For"
+// for (let index = 1; index <= 3; index += 1) {
+//     alert(index);
+// }
+
+// let languages = ['HTML', 'CSS', 'JavaScript'];
+// for (let index = 0; index < languages.length; index += 1) {
+//     alert('I love ' + languages[index] + '!');
+// }
+
+
+// let total = 0;
+// let summands = [1, 2, 3, 4, 5];
+// for (let index = 0; index < summands.length; index += 1) {
+//     total += summands[index];
+// }
+// alert(total); //15
+
+//for loop
+let total = 0;
+for (let currentNumber = 1; currentNumber <= 5; currentNumber += 1) {
+    total += currentNumber;
+}
+alert("Total is: " + total);
