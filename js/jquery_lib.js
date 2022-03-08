21ff2bcfdecb1bb12285bc852e45a9f1765a1438
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-08 13:22:01 +0300

    ft: intro to jQUERY complete

---
+++ js/jquery_lib.js
@@ -0,0 +1,20 @@
+//$(document).ready()
+//jQuery(document).ready(function(){}); // 
+
+// jQuery('h1').click(function(){
+//     alert('This is a heading tag')
+// });
+
+// jQuery('p').click(function(){
+//     alert('This is a paragraph')
+// });
+
+// jQuery('img').click(function(){
+//     alert('This is an image')
+// });
+
+//pop two alerts
+jQuery('h1').click(function(){
+    alert('This is a header.');
+    alert('I told you, THIS IS A HEADER');
+});
