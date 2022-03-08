caf0e641be26bde9624f750516cf850ea1781fa3
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-08 07:44:35 +0300

    ft: array mapping using forEach

--- js/looping_in_arrays.js
+++ js/looping_in_arrays.js
@@ -3,9 +3,23 @@
 //     alert('I love ' + language + '!');
 // });
 
-let total = 0;
-let summands = [1, 2, 3, 4, 5];
-summands.forEach(function(summand){
-    total += summand;
+// let total = 0;
+// let summands = [1, 2, 3, 4, 5];
+// summands.forEach(function(summand){
+//     total += summand;
+// });
+// alert(total); //15
+
+// let friends = ['Tuwei', 'Mwazo', 'Suleiman', 'Rhinolamwer'];
+// friends.forEach(function(friend) {
+//     alert('Bonjour ' + friend + '!');
+// });
+
+// Array : Mapping
+//Double numbers in an array using forEach()
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = [];
+numbers.forEach(function(number) {
+    doubledNumbers.push(number * 2);
 });
-alert(total); //15
+alert(doubledNumbers);
